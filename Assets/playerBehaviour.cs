4b059b8a5887f3607bf5ae084520236d63032e53
Daquaris Chadwick <chadwickd@knights.ucf.edu>
2016-01-16 00:32:34 -0500

    add main files

---
+++ Assets/playerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveCamera ();
+	}
+
+	//move player through arrow keys
+	void moveCamera ()
+	{
+		if (Input.GetKey (KeyCode.W))
+			transform.Translate (new Vector3 (0, 0, 1));
+		if (Input.GetKey (KeyCode.S))
+			transform.Translate (new Vector3 (0, 0, -1));
+		if (Input.GetKey (KeyCode.A))
+			transform.Translate (new Vector3 (-1, 0, 0));
+		if (Input.GetKey (KeyCode.D))
+			transform.Translate (new Vector3 (1, 0, 0));
+	}
+}
